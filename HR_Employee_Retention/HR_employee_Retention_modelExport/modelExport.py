7c3c6815672b0a4c150dd1cf679c3a451aa25379
AlteryxML <javier.lombana@alteryx.com>
2024-02-08 10:29:07 +0000

    Upated version of python

--- HR_Employee_Retention/HR_employee_Retention_modelExport/modelExport.py
+++ HR_Employee_Retention/HR_employee_Retention_modelExport/modelExport.py
@@ -167,7 +167,7 @@ pipeline = BinaryClassificationPipeline(
             "k_neighbors": 5,
         },
         "Logistic Regression Classifier": {
-            "penalty": "10",
+            "penalty": "15",
             "C": 1.0,
             "n_jobs": -1,
             "multi_class": "auto",
